cf9fdc77b93cfdcb26ad666befd88124c414035f
testuser1 <testuser1@ip-10-1-1-113.ec2.internal>
2019-05-12 05:03:51 +0000

    updated the script from ec2-testuser1

--- shell_scripting/script1.sh
+++ shell_scripting/script1.sh
@@ -9,9 +9,21 @@ echo 'hello world'
 # hostname as a variable 
 Y=$(hostname)
 
-echo "this is the hostname for this machine ${y}"
+echo this is the hostname for this machine "${Y}"
 
 # script executed by
-X=${whoami}
+X=$(whoami)
 
-echo "this script is executed by ${X}"
+echo this script is executed by "${X}"
+
+
+#list all users in the system
+# USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+
+# using awk command get the values for the first field under /etc/passwd
+
+USERS=$(awk -F : '{print $1}' /etc/passwd )
+
+echo "list all members : ${USERS}"
+
+
